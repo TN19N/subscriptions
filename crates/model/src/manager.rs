50eb8d0026aadf4a3c138b9a9fa185b57f9c03e8
Mustapha <mostafaanawawi@gmail.com>
2025-08-08 14:44:09 +0100

    setup

--- crates/model/src/manager.rs
+++ crates/model/src/manager.rs
@@ -1,11 +1,11 @@
-use crate::{Config, Result};
+use crate::{Config, Error, Result};
+use include_dir::{Dir, include_dir};
 use secrecy::ExposeSecret;
-use surrealdb::{
-    Surreal,
-    engine::any::Any,
-    opt::auth::{Database, Namespace, Root},
-};
+use surrealdb::{Surreal, engine::any::Any, opt::auth::Database};
+use surrealdb_migrations::MigrationRunner;
 use tokio::sync::OnceCell;
+
+const MIGRATIONS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/surrealdb");
 
 #[derive(Debug, Clone)]
 pub struct Manager {
@@ -21,7 +21,6 @@ impl Manager {
         }
     }
 
-    // #[tracing::instrument(skip_all)]
     async fn connect(&self) -> Result<Surreal<Any>> {
         let config = &self.config;
         let db = Surreal::<Any>::init();
@@ -29,30 +28,32 @@ impl Manager {
         tracing::info!("Connecting to database: {}", config.url.as_str());
         db.connect(config.url.as_str()).await?;
 
-        if let Some(namespace) = &config.namespace {
-            if let Some(name) = &config.name {
-                db.signin(Database {
-                    username: &config.username,
-                    password: config.password.expose_secret(),
-                    database: name,
-                    namespace,
-                })
+        if config.url.scheme() == "mem" {
+            db.query(format!("DEFINE NAMESPACE {}", config.namespace))
+                .query(format!("DEFINE DATABASE {}", config.name))
+                .query(format!("USE NS {} DB {}", config.namespace, config.name))
+                .query(format!(
+                    "DEFINE USER {} ON DATABASE PASSWORD '{}' ROLES OWNER",
+                    config.username,
+                    config.password.expose_secret()
+                ))
                 .await?;
-            } else {
-                db.signin(Namespace {
-                    username: &config.username,
-                    password: config.password.expose_secret(),
-                    namespace,
-                })
-                .await?;
-            }
-        } else {
-            db.signin(Root {
-                username: &config.username,
-                password: config.password.expose_secret(),
-            })
-            .await?;
-        };
+        }
+
+        db.signin(Database {
+            username: &config.username,
+            password: config.password.expose_secret(),
+            namespace: &config.namespace,
+            database: &config.name,
+        })
+        .await?;
+
+        // Apply Migrations
+        MigrationRunner::new(&db)
+            .load_files(&MIGRATIONS_DIR)
+            .up()
+            .await
+            .map_err(|e| Error::Migrations(e.to_string()))?;
 
         Ok(db)
     }
